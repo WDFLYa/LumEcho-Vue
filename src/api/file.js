d90b4b131ca3c343a37605099d01a95260eeb792
Wdfly <wangdf57487@hundsun.com>
2026-04-17 13:08:15 +0800

    修复编辑头像bug

--- src/api/file.js
+++ src/api/file.js
@@ -1,17 +1,33 @@
 import request from "@/utils/request"
 
 /**
- * 上传图片
- * @param {File} file - 图片文件
- * @param {String} bizType - 业务类型 (POST_IMAGE / AVATAR 等)
+ * 通用文件上传
+ * @param {File} file - 文件对象
+ * @param {String} bizType - 业务类型 (如：image, document, video 等)
  */
 export function uploadFile(file, bizType) {
-
     const formData = new FormData()
     formData.append("file", file)
     formData.append("bizType", bizType)
 
     return request.post("/api/upload", formData, {
+        headers: {
+            "Content-Type": "multipart/form-data"
+        }
+    })
+}
+
+/**
+ * 上传/更新用户头像
+ * @param {File} file - 头像文件
+ * @param {String} bizType - 业务类型 (默认：avatar)
+ */
+export function uploadAvatar(file, bizType = "avatar") {
+    const formData = new FormData()
+    formData.append("file", file)
+    formData.append("bizType", bizType)
+
+    return request.post("/api/avatar/upload", formData, {
         headers: {
             "Content-Type": "multipart/form-data"
         }
